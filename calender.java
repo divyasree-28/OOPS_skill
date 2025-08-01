2eb203940d09a4429e482e0139058a4f1edb2eb1
divyasree-28 <divyasreereddy90530@gmail.com>
2025-08-01 11:15:46 +0530

    Added file calendar.java

---
+++ calender.java
@@ -0,0 +1,18 @@
+package OOPS_Skill;
+import java.util.*;
+
+public class calender {
+	public static void main(String args[])
+	{
+		Calendar c=Calendar.getInstance();
+		Date d=c.getTime();
+		System.out.println(d);
+		c.set(2023,Calendar.SEPTEMBER,13);
+		int year=c.getWeekYear();
+		System.out.println(year);
+		int h=c.getFirstDayOfWeek();
+		System.out.println(h);
+	}
+
+
+}
